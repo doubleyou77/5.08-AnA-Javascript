0c26d680fd1773f51f2a964dc7b52a38207b5da5
double <21sunrin077@sunrint.hs.kr>
2022-05-08 15:47:30 +0900

    20220508

---
+++ exam.js
@@ -0,0 +1,45 @@
+const toCamelCase = '예시입니다.';
+
+//주석
+
+// /* 
+//   주석
+// */
+
+console.log('세미콜론을 생활화하자');
+
+console.log('메시지를 출력합니다');
+console.dir('객체의 속성을 출력합니다');
+console.error('오류 메시지를 출력합니다');
+
+let value;
+value = 1;
+value = 10;
+
+let decVariableLet;
+let variableLet = '초기값을 넣은 변수';
+const testvalue = 7;
+const testvalue2 = 'test';
+
+const intValue = 1;
+console.log(typeof(intValue));
+const floatValue = 1.2;
+console.log(typeof(floatValue));
+
+const stringValue = '문자열';
+console.log(typeof(stringValue));
+
+const booleanValue = 1 > 2;
+console.log(booleanValue);
+console.log(typeof(booleanValue));
+
+let undefinedValue;
+console.log(undefinedValue, typeof(undefinedValue));
+let nullValue = null;
+console.log(nullValue, typeof(nullValue));
+
+const ana = {
+  name: ['김형진', '김승태', '...'],
+  age: [18, 18, ],
+  
+}
